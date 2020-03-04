4350138e55fc3f8246f27d2e90de9085a5dc4b26
xinjie_guo <xinjie_guo@xinyan.com>
2020-03-04 10:49:57 +0800

    整合springboot-activiti-junit

---
+++ src/test/java/gxj/study/ActivitiTest.java
@@ -0,0 +1,44 @@
+package gxj.study;
+
+import gxj.study.activiti.config.SecurityUtil;
+import org.activiti.api.process.runtime.ProcessRuntime;
+import org.activiti.api.runtime.shared.query.Page;
+import org.activiti.api.runtime.shared.query.Pageable;
+import org.activiti.api.task.runtime.TaskRuntime;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author xinjie_guo
+ * @version 1.0.0 createTime:  2020/3/4 10:15
+ * @description
+ */
+public class ActivitiTest extends BaseTest {
+
+    @Autowired
+    private ProcessRuntime processRuntime;//实现流程定义相关操作
+
+    @Autowired
+    private TaskRuntime taskRuntime;//任务相关操作
+
+    @Autowired
+    private SecurityUtil securityUtil; //Security相关工具类
+
+    //流程定义信息 查看
+    @Test
+    public void testDefinition(){
+        //分页查询流程信息
+        //注意：流程部署工作在activiti7与springboot整合后，会自动部署 resource/processes/*.bpmn
+        Page processDefinitionPage = processRuntime.processDefinitions(Pageable.of(0,10));
+
+        //查看已部署流程个数
+        System.out.println(processDefinitionPage.getTotalItems());
+
+        //processDefinitionPage.getContent() 得到当前部署的每一个流程的信息
+        //pd的类型是ProcessDefinition
+        for(Object pd:processDefinitionPage.getContent()){
+            System.out.println(pd);
+        }
+    }
+
+}
--- src/test/java/gxj/study/BaseTest.java
+++ src/test/java/gxj/study/BaseTest.java
@@ -1,10 +1,16 @@
 package gxj.study;
 
+import gxj.study.activiti.config.SecurityUtil;
+import org.activiti.api.process.runtime.ProcessRuntime;
+import org.activiti.api.runtime.shared.query.Page;
+import org.activiti.api.runtime.shared.query.Pageable;
+import org.activiti.api.task.runtime.TaskRuntime;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 //import org.mybatis.spring.SqlSessionFactoryBean;
 //import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,6 +22,5 @@ import java.util.List;
 @SpringBootTest
 //@EnableAspectJAutoProxy(proxyTargetClass = false)
 public class BaseTest {
-
 
 }
